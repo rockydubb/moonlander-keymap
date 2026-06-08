788009e35901e8c953f94778d94f4d7868fc3a3a
Rocky Dubb <rockydubb@gmail.com>
2026-06-08 08:12:12 -0400

    ✨(qmk): let layer 0 RGB animations run free
    - keymap.c: rgb_matrix_indicators_user() returns early on layer 0,
      stopping the static ledmap[] from overwriting the QMK animation
      effect on every matrix scan
    - config.h: add RGB_MATRIX_DEFAULT_MODE 6 (CYCLE_LEFT_RIGHT rainbow)
      so an animation runs on boot
    
    Without this, the 'Switch ANI' / 'Animation' key (RGB_MODE_FORWARD)
    changes the effect but you can't see it because the per-key ledmap[]
    is reapplied hundreds of times per second.
    
    NOTE: Oryx syncs will wipe these edits. Re-apply after each sync
    (see docs/workflow.md rebase dance).

--- nvWgW/keymap.c
+++ nvWgW/keymap.c
@@ -18,24 +18,24 @@ enum tap_dance_codes {
   DANCE_0,
 };
 
-#define DUAL_FUNC_0 LT(7, KC_S)
-#define DUAL_FUNC_1 LT(1, KC_F13)
-#define DUAL_FUNC_2 LT(4, KC_F20)
-#define DUAL_FUNC_3 LT(2, KC_U)
-#define DUAL_FUNC_4 LT(10, KC_F8)
-#define DUAL_FUNC_5 LT(13, KC_B)
+#define DUAL_FUNC_0 LT(14, KC_F)
+#define DUAL_FUNC_1 LT(1, KC_F11)
+#define DUAL_FUNC_2 LT(9, KC_F10)
+#define DUAL_FUNC_3 LT(7, KC_G)
+#define DUAL_FUNC_4 LT(7, KC_W)
+#define DUAL_FUNC_5 LT(5, KC_L)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     MT(MOD_LALT | MOD_LCTL, KC_ESCAPE),KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_AUDIO_VOL_DOWN,                                KC_AUDIO_VOL_UP,KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           DUAL_FUNC_4,    
-    TD(DANCE_0),    KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TAB,                                         TG(1),          KC_Y,           KC_U,           KC_I,           KC_O,           DUAL_FUNC_5,    KC_BSPC,        
+    TD(DANCE_0),    KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           RGB_MODE_FORWARD,                                TG(1),          KC_Y,           KC_U,           KC_I,           KC_O,           DUAL_FUNC_5,    KC_BSPC,        
     ALL_T(KC_CAPS), KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_EQUAL,                                                                       TO(2),          KC_H,           KC_J,           KC_K,           KC_L,           LT(2, KC_SCLN), MT(MOD_LGUI, KC_ENTER),
     KC_LEFT_SHIFT,  DUAL_FUNC_0,    DUAL_FUNC_1,    DUAL_FUNC_2,    DUAL_FUNC_3,    KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RIGHT_SHIFT, 
     KC_LEFT_CTRL,   CW_TOGG,        KC_LEFT_ALT,    KC_LEFT,        KC_RIGHT,       RGB_MODE_FORWARD,                                                                                                RSFT(KC_RIGHT_ALT),KC_UP,          KC_DOWN,        KC_LBRC,        KC_RBRC,        KC_RIGHT_CTRL,  
     KC_SPACE,       KC_LEFT_ALT,    KC_LEFT_GUI,                    KC_LEFT_ALT,    KC_TAB,         KC_SPACE
   ),
   [1] = LAYOUT_moonlander(
-    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         AC_TOGG,         
+    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         
     KC_TAB,         KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_UP,          KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_MINUS,    KC_F12,         
     KC_TRANSPARENT, KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRAVE,       KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_DOWN,        KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_KP_PLUS,     KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_CIRC,        KC_LBRC,        KC_RBRC,        KC_TILD,                                        KC_AMPR,        KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_KP_SLASH,    KC_TRANSPARENT, 
